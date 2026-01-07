3064cc265f8a8449779d10d4119d1bba6e95cd6b
iVAN <88724353+ivan-hc@users.noreply.github.com>
2026-01-07 23:59:19 +0100

    Update lxtask-anylinux.sh - remove libgallium

--- lxtask-anylinux.sh
+++ lxtask-anylinux.sh
@@ -9,7 +9,7 @@ DEPENDENCES="" #SYNTAX: "APP1 APP2 APP3 APP4...", LEAVE BLANK IF NO OTHER DEPEND
 # Set keywords to searchan include in names of directories and files in /usr/bin (BINSAVED), /usr/share (SHARESAVED) and /usr/lib (LIBSAVED)
 BINSAVED="SAVEBINSPLEASE"
 SHARESAVED="SAVESHAREPLEASE"
-LIBSAVED="SAVELIBSPLEASE"
+LIBSAVED="libgallium"
 
 # Set the items you want to manually REMOVE. Complete the path in /etc/, /usr/bin/, /usr/lib/, /usr/lib/python*/ and /usr/share/ respectively.
 # The "rm" command will take into account the listed object/path and add an asterisk at the end, completing the path to be removed.
